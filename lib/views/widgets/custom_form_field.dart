51d503e9c6aa1590096599599ffbe875134e378b
adogbeji <baka_1993@yahoo.co.uk>
2024-09-27 19:56:45 +0100

    Add starting code for CustomFormField

---
+++ lib/views/widgets/custom_form_field.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomFormField extends StatelessWidget {
+  const CustomFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Custom Form Field'),
+      ),
+    );
+  }
+}
