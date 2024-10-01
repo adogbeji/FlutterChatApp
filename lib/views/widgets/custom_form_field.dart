998f4bdad27454b8b708cc67e5b6093f185ce562
adogbeji <baka_1993@yahoo.co.uk>
2024-10-01 19:53:00 +0100

    Start setting parameters on TextFormField in CustomFormField

--- lib/views/widgets/custom_form_field.dart
+++ lib/views/widgets/custom_form_field.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomFormField extends StatelessWidget {
-  const CustomFormField({super.key});
+  const CustomFormField({super.key, required this.labelText});
+
+  final String labelText;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: OutlineInputBorder(),
+      ),
+    );
   }
 }
