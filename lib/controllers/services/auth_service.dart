12f8339c7de9bc7d96d63e51dc302c340a0c60f2
adogbeji <baka_1993@yahoo.co.uk>
2024-10-14 19:02:31 +0100

    Create variable to store firebase_auth package & start adding code to log in users

--- lib/controllers/services/auth_service.dart
+++ lib/controllers/services/auth_service.dart
@@ -1,5 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthService {
   AuthService() {}
 
-  Future<bool> login(String email, String password) {}
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;  // Stores firebase_auth package
+
+  Future<bool> login(String email, String password) {
+    try {
+      _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {}
+  }
 }
