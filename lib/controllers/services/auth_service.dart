643811f11598e9cb744b7931685ec328d64158cc
adogbeji <baka_1993@yahoo.co.uk>
2024-10-15 19:09:03 +0100

    Create variable to store login credentials in AuthService class

--- lib/controllers/services/auth_service.dart
+++ lib/controllers/services/auth_service.dart
@@ -5,9 +5,13 @@ class AuthService {
 
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;  // Stores firebase_auth package
 
-  Future<bool> login(String email, String password) {
+  Future<bool> login(String email, String password) async {
     try {
-      _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      final credential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+
+      if (credential.user != null) {
+        return true;
+      }
     } catch (e) {}
   }
 }
