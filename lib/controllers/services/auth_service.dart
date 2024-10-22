0446eda8fcfc74467bc690bcee54bab9ffe1bef5
adogbeji <baka_1993@yahoo.co.uk>
2024-10-22 20:31:58 +0100

    Define getter in AuthService class

--- lib/controllers/services/auth_service.dart
+++ lib/controllers/services/auth_service.dart
@@ -6,6 +6,10 @@ class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;  // Stores firebase_auth package
   User? _user;  // Stores reference for logged in user
 
+  User? get user {
+    return _user;
+  }
+
   Future<bool> login(String email, String password) async {
     try {
       final credential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
