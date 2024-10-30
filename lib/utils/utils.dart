b27570f3888571f1960463ae323a1066338380d3
adogbeji <baka_1993@yahoo.co.uk>
2024-10-30 10:41:48 +0000

    Correct typo in comment in utils.dart file

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,5 +4,5 @@ import 'package:get_it/get_it.dart';
 Future<void> registerServices() async {
   final GetIt getIt = GetIt.instance;  // Stores get_it package
 
-  getIt.registerSingleton<AuthService>(AuthService());  // Register instance opf AuthService class
+  getIt.registerSingleton<AuthService>(AuthService());  // Register instance of AuthService class
 }
