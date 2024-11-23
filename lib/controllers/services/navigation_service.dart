45196b59c8bff3ef69d6853e5e07b07c7621f093
adogbeji <baka_1993@yahoo.co.uk>
2024-11-23 09:18:51 +0000

    Start defining routes in NavigationService class

--- lib/controllers/services/navigation_service.dart
+++ lib/controllers/services/navigation_service.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:chat_app/views/screens/auth/login_screen.dart';
 
 class NavigationService {
-
   late GlobalKey<NavigatorState> _navigatorKey;
+
+  NavigationService() {
+    _navigatorKey = GlobalKey();
+  }
+
+  final Map<String, Widget Function(BuildContext)> _routes = {
+    '/login': (context) => const LoginScreen(),
+  };
 }
