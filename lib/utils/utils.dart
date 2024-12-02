08edc1b77d3cddcba910e8537c56ad1ea68a2849
adogbeji <baka_1993@yahoo.co.uk>
2024-12-02 20:43:27 +0000

    Register NavigationService class

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,8 +1,10 @@
 import 'package:chat_app/controllers/services/auth_service.dart';
+import 'package:chat_app/controllers/services/navigation_service.dart';
 import 'package:get_it/get_it.dart';
 
 Future<void> registerServices() async {
   final GetIt getIt = GetIt.instance;  // Stores get_it package
 
   getIt.registerSingleton<AuthService>(AuthService());  // Register instance of AuthService class
+  getIt.registerSingleton<NavigationService>(NavigationService());  // Register instance of NavigationService class
 }
